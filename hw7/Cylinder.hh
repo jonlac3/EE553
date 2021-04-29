84a146aad9fa571058e3843eaac4d065e17bd0db
Jonas Lackey <54653242+jonlac3@users.noreply.github.com>
2021-04-29 19:34:45 -0400

    Update Cylinder.hh

--- hw7/Cylinder.hh
+++ hw7/Cylinder.hh
@@ -7,5 +7,6 @@ protected:
     double x, y, z, r, h, facets;
 public:
     Cylinder(double x, double y, double z, double r, double h, double facets): x(x), y(y), z(z), r(r), h(h), facets(facets){}
-    void print(string filename) const;
+
+    void print(string file) const;
 };
